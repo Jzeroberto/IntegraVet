942379f1048953a96ed35a7f0ba8aabb203fcce1
Jose Roberto <jose.roberto2002@hotmail.com>
2024-08-13 20:44:07 -0300

    Pagina principal

--- configuration.py
+++ configuration.py
@@ -1,5 +1,6 @@
 from routes.login import login_route
 from routes.register import register_route
+from routes.menu import menu_route
 
 
 def configure_all(app):
@@ -9,3 +10,4 @@ def configure_all(app):
 def configure_routes(app):
     app.register_blueprint(login_route)
     app.register_blueprint(register_route)
+    app.register_blueprint(menu_route)
