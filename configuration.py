017cc5bf6da48616cc6ce2323a8e503e380428fd
Jzeroberto <jose.roberto2002@hotmail.com>
2024-08-13 18:06:40 -0300

    Tela de Cadastro

--- configuration.py
+++ configuration.py
@@ -1,8 +1,11 @@
 from routes.login import login_route
+from routes.register import register_route
 
 
 def configure_all(app):
     configure_routes(app)
 
+
 def configure_routes(app):
     app.register_blueprint(login_route)
+    app.register_blueprint(register_route)
