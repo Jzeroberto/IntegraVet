6658fd49a4ce2e069ec7fbc176e7b61b1b8c7854
Jzeroberto <jose.roberto2002@hotmail.com>
2025-02-27 21:44:34 -0300

    Correções

--- routes/principal.py
+++ routes/principal.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 principal_route = Blueprint('principal', __name__)
 
-@principal_route.route('/principal')
+@principal_route.route('/principal', methods=['POST'])
 def principal():
     """tela principal"""
     return render_template('principal.html')
